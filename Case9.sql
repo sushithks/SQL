b667f2b91e7928d9f7a3049f6d562ef9946ddabe
sushith <sushithks@gmail.com>
2025-02-28 16:27:40 -0500

    Case-9

---
+++ Case9.sql
@@ -0,0 +1,43 @@
+-------------------- Question ----------------------
+
+--Imagine you manage two  Visits and Transactions:
+-- Visits : "visit_id"  (integer), "customer_id"  (integer)
+-- Transactions :   "transaction_id"  (integer), "visit_id"  (integer), "amount" (integer)
+
+-- Question : Write a SQL query to list users who had visits without any transactions and how many times they visited?
+
+
+-- Table Creation --
+CREATE TABLE Visits (
+    visit_id INT,
+    customer_id INT
+);
+
+CREATE TABLE Transactions (
+    transaction_id INT,
+    visit_id INT,
+    amount INT
+);
+
+
+-- Data Insert --
+
+INSERT INTO Visits (visit_id, customer_id) VALUES
+(1, 23),
+(2, 9),
+(4, 30),
+(5, 54),
+(6, 96),
+(7, 54),
+(8, 54);
+
+
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES
+(2, 5, 310),
+(3, 5, 300),
+(9, 5, 200),
+(12, 1, 910),
+(13, 2, 970);
+
+-- Query --
+
