342e2a51d85442ef987b8f79cf03dac15aade2c5
sushith <sushithks@gmail.com>
2025-03-03 22:38:22 -0500

    case 11

---
+++ Case11.sql
@@ -0,0 +1,28 @@
+-------------------- Question ----------------------
+
+--Imagine you manage a table Activity:
+--Activity: "machine_id" (integer), "process_id" (date), "activity_typ" (string), "timestamp" (float).
+
+
+--Write a sql query to find all dates with higher temperatures compared to its previous dates (yesterday).
+
+
+-- Table Creation --
+CREATE TABLE Weather (
+    id INT,
+    recordDate DATE,
+    temperature INT
+);
+
+
+-- Data Insert --
+
+INSERT INTO Weather (id, recordDate, temperature) VALUES
+(1, '2025-02-28', 20),
+(2, '2025-03-01', 22),
+(3, '2025-03-02', 19),
+(4, '2025-03-03', 23),
+(5, '2025-03-04', 21),
+(6, '2025-03-05', 25),
+(7, '2025-03-06', 27),
+(8, '2025-03-07', 26);
