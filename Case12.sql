654b8bdc67ca53037fda2b50f225053f1d05a0e7
sushith <sushithks@gmail.com>
2025-03-04 20:05:40 -0500

    case 12 main

--- Case12.sql
+++ Case12.sql
@@ -61,3 +61,25 @@ INSERT INTO Examinations (student_id, subject_name) VALUES
 
 
 -- Query --
+
+
+SELECT
+    stu.student_id,
+    stu.student_name,
+    sub.subject_name,
+    COUNT(ex.subject_name) AS attended_exams
+FROM
+    Students stu
+CROSS JOIN
+    Subjects sub
+LEFT JOIN
+    Examinations ex
+ON
+    stu.student_id = ex.student_id AND
+    sub.subject_name = ex.subject_name
+GROUP BY
+    stu.student_id,
+    stu.student_name,
+    sub.subject_name
+ORDER BY
+    stu.student_id
