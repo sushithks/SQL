992d6f576fe25b181305648c6d46a600bd26e3b9
sushith <sushithks@gmail.com>
2025-03-15 21:30:07 -0400

    case 20 main

--- Case20.sql
+++ Case20.sql
@@ -32,3 +32,15 @@ VALUES
     (4, 3, '2019-06-25', 'end_session');
 
 -- Query --
+
+SELECT product_id,
+    year AS first_year,
+    quantity,
+    price
+FROM
+    sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM sales
+    GROUP BY product_id
+)
