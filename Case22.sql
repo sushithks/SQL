a256aa6410028d06919d94d534662b7556162cec
sushith <sushithks@gmail.com>
2025-03-17 19:27:55 -0400

    case 22

---
+++ Case22.sql
@@ -0,0 +1,28 @@
+-------------------- Question ----------------------
+
+--Imagine you manage a table Employees:
+--Employees: "employee_id" (integer), "department_id" (integer), "primary_flag" (String)
+
+
+--Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+
+
+-- Table Creation --
+
+-- Create the Employees table
+create table Employees(
+	employee_id   INTEGER,
+    department_id INTEGER,
+    primary_flag  varchar(10))
+
+
+-- Insert data into Employees table
+INSERT INTO Employees (employee_id, department_id, primary_flag)
+VALUES
+    (1, 1, 'N'),
+    (2, 1, 'Y'),
+    (2, 2, 'N'),
+    (3, 3, 'N'),
+    (4, 2, 'N'),
+    (4, 3, 'Y'),
+    (4, 4, 'N');
