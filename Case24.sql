c34033b6b2d4143864828153f958c98b5396d4c8
sushith <sushithks@gmail.com>
2025-03-20 17:49:04 -0400

    case 24 main-2

--- Case24.sql
+++ Case24.sql
@@ -39,3 +39,19 @@ from
     num_table
 where
     num = next_num = last_num
+
+
+------------------------- Query 2 --------------------------------
+
+select
+    distinct l1.num
+from
+    logs l1,
+    logs l2,
+    logs l3
+where
+    l1.Id = l2.Id - 1
+    and l2.Id = l3.Id - 1
+    and l1.num = l2.num
+    and l2.num = l3.num
+
