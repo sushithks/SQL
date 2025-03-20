7837e74d0af939d59c02c8ba6e0f234cc34c4935
sushith <sushithks@gmail.com>
2025-03-20 17:48:48 -0400

    case 24 main-1

--- Case24.sql
+++ Case24.sql
@@ -22,3 +22,20 @@ VALUES
     (5, 1),
     (6, 2),
     (7, 2);
+
+------------------------- Query 1 --------------------------------
+
+with num_table as(
+    select
+        num,
+        lead(num,1) over (order by num) as next_num,
+        lag(num,1) over (order by num) as last_num
+    from logs
+)
+
+select
+    distinct num
+from
+    num_table
+where
+    num = next_num = last_num
