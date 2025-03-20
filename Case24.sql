3cd5c30f78ab59895ffac2690660bb86be7931ab
sushith <sushithks@gmail.com>
2025-03-20 17:49:14 -0400

    case 24 main-3

--- Case24.sql
+++ Case24.sql
@@ -55,3 +55,20 @@ where
     and l1.num = l2.num
     and l2.num = l3.num
 
+
+------------------------- Query 3 --------------------------------
+
+select
+    distinct l1.num
+from
+    logs l1
+join
+    logs l2
+on
+    l1.num = l2.num
+    and l1.id = l2.id -1
+join
+    logs l3
+ on
+    l2.num = l3.num
+    and l2.id = l3.id -1
