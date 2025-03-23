7157003f2687643f2e0dd18d976b8ed07fef0285
sushith <sushithks@gmail.com>
2025-03-23 18:50:58 -0400

    case 25 part-1

--- Case25.sql
+++ Case25.sql
@@ -26,3 +26,20 @@ VALUES
     (2, 65, '2019-08-17'),
     (3, 20, '2019-08-18');
 
+
+------------------------- Query 1 --------------------------------
+
+WITH has_price AS (
+    SELECT
+        product_id,
+            IFNULL(new_price,10) AS price
+    FROM
+        Products
+    WHERE
+        (product_id, change_date) IN (
+            SELECT product_id, MAX(change_date)
+            FROM Products
+            WHERE change_date <= DATE('2019-08-16')
+            GROUP BY product_id
+            )
+)
