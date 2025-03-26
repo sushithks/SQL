1e442aa114f6ff1680b18bc439cd1bdc21628f8d
sushith <sushithks@gmail.com>
2025-03-25 21:11:17 -0400

    case 27 main

--- Case27.sql
+++ Case27.sql
@@ -25,3 +25,40 @@ INSERT INTO accounts (account_id, income) VALUES
 (2, 12747),
 (8, 87709),
 (6, 91796);
+
+
+
+------------------------- Query 1 --------------------------------
+
+with low as(
+    select
+        case when 1=1 then 'Low Salary' end as category,
+        count(*)  as accounts_count
+    from
+        accounts
+    where
+        income < 20000
+),
+Average as (
+    select
+        case when 1=1 then 'Average Salary' end as category,
+        count(*)  as accounts_count
+    from
+        accounts
+    where
+        income between 20000 and 50000
+),
+High as (
+    select
+        case when 1=1 then 'High Salary' end as category,
+        count(*)  as accounts_count
+    from
+        accounts
+    where
+        income > 50000
+)
+select * from low
+UNION
+select * from Average
+UNION
+select * from High
