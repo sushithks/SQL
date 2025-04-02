af221473fe7733150c6cafaa89d568afeccf2a79
sushith <sushithks@gmail.com>
2025-04-02 19:41:13 -0400

    case 32 main-2

--- Case32.sql
+++ Case32.sql
@@ -43,3 +43,24 @@ WHERE
     loc_count = 1
 
 
+
+
+------------------------- Query 2 --------------------------------
+
+SELECT
+    round(SUM(tiv_2016), 2) as tiv_2016
+FROM
+    Insurance
+WHERE
+    tiv_2015 IN(
+        SELECT tiv_2015
+        FROM Insurance
+        GROUP BY tiv_2015
+        HAVING COUNT(*) >1
+    )
+    AND (lat,lon) IN (
+        SELECT lat,lon
+        FROM Insurance
+        GROUP BY lat,lon
+        HAVING COUNT(*)=1
+    )
