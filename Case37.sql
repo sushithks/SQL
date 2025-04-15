ffd7de88a0556c01ffdb5943cd1eb4e922ac09dc
sushith <sushithks@gmail.com>
2025-04-14 20:15:19 -0400

    case 37

---
+++ Case37.sql
@@ -0,0 +1,30 @@
+-------------------- Question ----------------------
+
+--Imagine you manage a table Employee :
+--Employee: "id" (integer), "salary" (integer)
+
+--Question :-- Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null
+
+------------------ Table Creation --------------------
+
+-- Create the Employee table
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    salary INT
+);
+
+
+-- Insert data into Employee table
+INSERT INTO employee (id, salary) VALUES
+(1, 75000),
+(2, 85000),
+(3, 90000),
+(4, 65000),
+(5, 72000),
+(6, 85000),
+(7, 78000),
+(8, 91000),
+(9, 88000),
+(10, 70000);
+
+
